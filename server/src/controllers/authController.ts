37fae0edd24b4c49fc50012937e7f312bf38a675
WillQizza <williamqipizza@gmail.com>
2023-07-24 22:42:01 -0600

    add migrations

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -3,7 +3,12 @@ import { Request, Response } from "express";
 import { getConnection } from "../database";
 
 export async function onTokenRegister(req : Request, res : Response) {
-  
+  const connection = await getConnection();
+  try {
+
+  } finally {
+    connection.release();
+  }
 }
 
 export function onTokenAuthorize(req : Request, res : Response) {
